3cc044fb5724b111c232988bb2020eb81c9a9532
JanElmirov <99734651+JanElmirov@users.noreply.github.com>
2022-02-25 18:34:05 +0600

    Add files via upload

---
+++ bdsgscvk/main.js
@@ -0,0 +1,41 @@
+// console.log(5 > 10);
+// console.log("ананас" > "яблоко");
+// console.log("2" > "12");
+
+
+// console.log(undefined === null);
+// console.log(indefined == "\10\2");
+
+// let num = 6.35
+// console.log(Math.round(6.35));
+
+
+//  4, -2, 5, 19, -130, 0, 10
+//  console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+//  console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+// console.log(Math.floor(Math.random() * 100));
+
+let jan = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let i =0
+while (i < 10) {
+	jan[i].toString()
+	console.log(jan[i]);
+	++i
+}
+
+// const arr = [ 1, '2', '3', '4']
+// console.log(+true);
+// console.log(+false);
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Родители разрешили?');
+//   }
+// }
+// if (year == 2015) {
+//   alert( "Правильно!" );
+//   alert( "Вы такой умный!" );
+// }
